130fc6091954c8e79592f01d6646067d58975a2b
itssinghankit <singhankit.kr@gmail.com>
2024-05-28 14:32:59 +0530

    chore: Update app.js to fix CORS configuration

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const corsConfig={
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 };
-app.options("*", corsConfig);
+app.options("", corsConfig);
 app.use(cors(corsConfig));
 
 app.use(express.json({ limit: "16kb" }))
