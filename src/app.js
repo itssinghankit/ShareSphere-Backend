2f6b8519c4196c6e51b1864691b5dbe4cf852b37
itssinghankit <singhankit.kr@gmail.com>
2025-04-30 01:22:57 +0530

    pushing final notification functionality

--- src/app.js
+++ src/app.js
@@ -33,7 +33,6 @@ const io = new Server(httpServer, {
 
 app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global`
 
-
 const corsConfig = {
     origin: process.env.CORS_ORIGIN,
     credentials: true
